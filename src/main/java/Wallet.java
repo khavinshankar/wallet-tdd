d1069750c3e032a131e65d8d2bf5917c92b5ae57
Khavin Shankar <khavinshankar@gmail.com>
2021-11-06 11:07:19 +0530

    feat: implemented requirement 2, added deposit and getBalance in Wallet

--- src/main/java/Wallet.java
+++ src/main/java/Wallet.java
@@ -1,6 +1,16 @@
 public class Wallet {
     private final double DOLLARTORUPEES = 74.85;
 
+    private double balance = 0;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
     public double rupeesToDollars(double rupees) {
         return rupees / DOLLARTORUPEES;
     }
--- src/test/java/WalletTest.java
+++ src/test/java/WalletTest.java
@@ -10,6 +10,29 @@ public class WalletTest {
     }
 
 //    As a wallet owner I would like to be able to put money into my wallet so that I can take it out later.
+    @Test
+    public void verifyDepositFunctionalityWithSingleDeposit() {
+        Wallet wallet = new Wallet();
+
+        wallet.deposit(50);
+        Assertions.assertEquals(50, wallet.getBalance());
+    }
+
+    @Test
+    public void verifyDepositFunctionalityWithMultipleDeposits() {
+        Wallet wallet = new Wallet();
+
+        wallet.deposit(50);
+        Assertions.assertEquals(50, wallet.getBalance());
+
+        wallet.deposit(150);
+        Assertions.assertEquals(200, wallet.getBalance());
+
+        wallet.deposit(100);
+        wallet.deposit(20);
+        Assertions.assertEquals(320, wallet.getBalance());
+    }
+
 //    As a wallet owner, I want to know the total money my wallet has for the preferred currency. When Rs.50 & $1 added to my wallet, Then the sum of money in the wallet is Rs.124.85, (Given preferred currency is Rs.)
 //    As a wallet owner, I want to know the total money my wallet has for the preferred currency. When Rs.74.85, $1, Rs.149.7 added to my wallet, Then sum of money in the wallet is $4, (Given preferred currency is $)
 //    As a wallet owner I would like to be able to take a specified amount of money out of the wallet.
